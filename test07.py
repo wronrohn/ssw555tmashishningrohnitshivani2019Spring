103e9116e74e48e0531eac13f14cad2187956037
Ashish Negi <ashishnegi2110@gmail.com>
2019-02-25 13:34:08 -0500

    changes in user story format and line numbers added with invalid

--- test07.py
+++ test07.py
@@ -5,28 +5,28 @@ class testcase(unittest.TestCase):
 
     def test_mbd(self):
         result=us_07(['1996-10-21',4],['2150-10-21',6],154,'I1')
-        self.assertEqual(result,"Error US07:Age of person is 150 years with I1 id")
+        self.assertEqual(result,"Error US07 in line 4 or line 6:Age of person is 150 years with I1 id")
 
         result=us_07('NA',['1994-04-03',6],'NA','I1')
-        self.assertEqual(result,"Warning US07:Birth date is not given in I1 id")
-
-        result=us_07(['1994-06-12',4],'NA','NA','I1')
         self.assertEqual(result,0)
 
+        result=us_07(['1994-06-12',4],'NA',154,'I1')
+        self.assertEqual(result,"Error US07 in line 4:Age of person is 150 years with I1 id")
+
         result=us_07('NA','NA','NA','I1')
-        self.assertEqual(result,"Warning US07:Birth date is not given in I1 id")
+        self.assertEqual(result,0)
 
-        result=us_07('NA','Invalid','Invalid','I1')
-        self.assertEqual(result,"Warning US07:Birth date is not given and Death date is invalid in I1 id")
+        result=us_07('NA',['Invalid',9],'NA','I1')
+        self.assertEqual(result,0)
 
-        result=us_07('Invalid','Invalid','Invalid','I1')
-        self.assertEqual(result,"Warning US07:Birth date and Death date is invalid in I1 id")
+        result=us_07(['Invalid',7],['Invalid',9],'Invalid','I1')
+        self.assertEqual(result,0)
 
-        result=us_07(['1994-06-12',4],'Invalid','Invalid','I1')
-        self.assertEqual(result,"Warning US07:Death date is invalid in I1 id")
+        result=us_07(['1994-06-12',4],['Invalid',8],'Invalid','I1')
+        self.assertEqual(result,0)
 
         result=us_07(['1947-04-15',4],['1900-04-15',6],-47,'I1')
-        self.assertEqual(result,"Error US07 in line 4:Birth date is after death date with I1 id")
+        self.assertEqual(result,0)
 
 if __name__=='__main__':
     unittest.main()
--- us07_an.py
+++ us07_an.py
@@ -1,26 +1,14 @@
 def us_07(birth,death,Age,i):
     if Age!='NA' and Age!='Invalid':
         if Age>=150:
-            return "Error US07:Age of person is 150 years with "+ i +" id"
-        elif Age<0:
-            return "Error US07 in line "+ str(birth[1]) + ":Birth date is after death date with "+ i +" id"
+            if death!='NA':
+                return "Error US07 in line "+ str(birth[1]) +" or line "+ str(death[1])+":Age of person is 150 years with "+ i +" id"
+            else:
+                return "Error US07 in line "+ str(birth[1]) +":Age of person is 150 years with "+ i +" id"
         else:
             return 0
     else:
-        if birth=='NA':
-            if death=="Invalid":
-                return "Warning US07:Birth date is not given and Death date is invalid in "+ i +" id"
-            else:
-                return "Warning US07:Birth date is not given in "+ i +" id"
-        elif birth=='Invalid':
-            if death=="Invalid":
-                return "Warning US07:Birth date and Death date is invalid in "+ i +" id"
-            else:
-                return "Warning US07:Birth date is invalid in "+ i +" id"
-        elif death=='Invalid':
-                return "Warning US07:Death date is invalid in "+ i +" id"
-        else:
-            return 0
+        return 0
 
 def parse_data_07(ind):
     for x in ind:
