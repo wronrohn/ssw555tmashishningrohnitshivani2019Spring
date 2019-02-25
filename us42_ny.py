9b1c2c4e34544f1caaae7fbfb488f0171fcaf1d9
Ning YANG <yndlut@gmail.com>
2019-02-25 12:56:18 -0500

    User story #42 Update argument input according to latest dictionary structure.

--- us42_ny.py
+++ us42_ny.py
@@ -9,17 +9,17 @@ def us42_tsk01_is_legit_date(dateString):
 
 def us42_legit_date(ind, family):
     for key, values in ind.items():
-        if (values.__contains__("BIRT_DATE") and ind[key]["BIRT_DATE"] == "Invalid"):
-            print('Error US42: Birth date of', ind[key]["NAME"], '(', key ,') is illegitimate.')
-        if (values.__contains__("DEAT_DATE") and ind[key]["DEAT_DATE"] == "Invalid"):
-            print('Error US42: Death date of', ind[key]["NAME"], '(', key ,') is illegitimate.')
+        if (values.__contains__("BIRT_DATE") and ind[key]["BIRT_DATE"][0] == "Invalid"):
+            print('Error US42 in line',ind[key]["BIRT_DATE"][1],': Birth date of', ind[key]["NAME"], '(', key ,') is illegitimate.')
+        if (values.__contains__("DEAT_DATE") and ind[key]["DEAT_DATE"][0] == "Invalid"):
+            print('Error US42 in line',ind[key]["DEAT_DATE"][1],': Death date of', ind[key]["NAME"], '(', key ,') is illegitimate.')
 
     for key, values in family.items():
-        if (values.__contains__("MARR_DATE") and family[key]["MARR_DATE"] == "Invalid"):
-            husID = family[key]["HUSB"]
-            wifeID = family[key]["WIFE"]
-            print('Error US42: Marriage date of ', ind[husID]["NAME"],'(', husID, ') and', ind[wifeID]["NAME"],'(', wifeID,') in Family (', key,') is illegitimate.')
-        if (values.__contains__("DIV_DATE") and family[key]["DIV_DATE"] == "Invalid"):
-            husID = family[key]["HUSB"]
-            wifeID = family[key]["WIFE"]
-            print('Error US42: Divorce date of ', ind[husID]["NAME"],'(', husID, ') and', ind[wifeID]["NAME"],'(', wifeID,') in Family (', key,') is illegitimate.')
+        if (values.__contains__("MARR_DATE") and family[key]["MARR_DATE"][0] == "Invalid"):
+            husID = family[key]["HUSB"][0]
+            wifeID = family[key]["WIFE"][0]
+            print('Error US42 in line',family[key]["MARR_DATE"][1],': Marriage date of ', ind[husID]["NAME"][0],'(', husID, ') and', ind[wifeID]["NAME"][0],'(', wifeID,') in Family (', key,') is illegitimate.')
+        if (values.__contains__("DIV_DATE") and family[key]["DIV_DATE"][0] == "Invalid"):
+            husID = family[key]["HUSB"][0]
+            wifeID = family[key]["WIFE"][0]
+            print('Error US42 in line', family[key]["DIV_DATE"][1],': Divorce date of ', ind[husID]["NAME"][0],'(', husID, ') and', ind[wifeID]["NAME"][0],'(', wifeID,') in Family (', key,') is illegitimate.')
